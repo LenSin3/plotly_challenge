290f5251fee51f45673a4fb8b9cc9220cb6ac345
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-02 05:26:46 -0800

    Initialized the default plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,17 @@
 // Create empty dictionaries to hold sample_values, otu_ids, otu_labels,
 
-
+var samplesData = [];
 // Load samples.json
 
 d3.json("/data/samples.json").then((data) => {
      
     // Extract samples data
-    var samples_data = data.samples;    
-    console.log(samples_data);
+    // var samples_data = data.samples; 
+    samplesData.push(data.samples);   
+    console.log(data.samples);
 
-    samples_data.forEach(element => {
-        var id = element.id.slice(0, 10);
+    /*samples_data.forEach(element => {
+        var id = element.id;
         console.log(id);
 
         var otu_ids = element.otu_ids.slice(0, 10);
@@ -24,8 +25,17 @@ d3.json("/data/samples.json").then((data) => {
         var otu_labels = element.otu_labels.slice(0, 10);
         console.log(otu_labels);
 
+        function init() {
+        data = [{
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16] }];
+
+        Plotly.newPlot("plot", data);
+}
+
 
     });
+    */
 
     // Grab subject ids
     // var id =  samples_data.map(val => val.id);
@@ -65,5 +75,37 @@ d3.json("/data/samples.json").then((data) => {
     */
 });
 
+console.log(samplesData)
 
+// Initializes the page with a default plot
+function init() {
+
+
+    samplesData.forEach(element => {
+        var id = element.id;
+        console.log(id);
+
+        var otu_ids = element.otu_ids;
+        console.log(otu_ids);
+
+        // Grab the sample values
+        var sample_values = element.sample_values;
+        console.log(sample_values);
+
+        // Grab the otu labels
+        var otu_labels = element.otu_labels;
+        console.log(otu_labels);
+
+
+    });
+
+
+    /*data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+  
+    Plotly.newPlot("plot", data);
+    */
+  };
+init();
 
