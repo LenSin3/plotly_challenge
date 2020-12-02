825ff022deac6bc6b4163def5f64398faa7838f4
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-02 00:43:05 -0800

    created relative path for js.app in index.html, read samples.json using d3

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,20 @@
 // Create empty dictionaries to hold sample_values, otu_ids, otu_labels,
-var sample_values = {};
-var otu_ids = {};
-var otu_labels = {};
+
 
 // Load samples.json
 
-d3.json("/data/samples.json", function(data) {
-    sample_values.push(data.sample_values);
+d3.json("/data/samples.json").then((data) => {
+     
+    var samples_data = data.samples;
+    // var sample_values = samples_data.id.sample_values;
+    console.log(samples_data);
+    // console.log(data.sample_values);
+    var otu_ids = samples_data.map(val => val.otu_ids);
+    console.log(otu_ids);
+    
+    
 
 });
+
+
+
