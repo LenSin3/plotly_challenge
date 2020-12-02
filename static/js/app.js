1e968a9573396d6d5f1b6ec3ead897048bd6d920
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-02 00:57:46 -0800

    Grabbed values for id, otu_ids, sample_values and sample_labels

--- static/js/app.js
+++ static/js/app.js
@@ -5,13 +5,26 @@
 
 d3.json("/data/samples.json").then((data) => {
      
-    var samples_data = data.samples;
-    // var sample_values = samples_data.id.sample_values;
+    // Extract samples data
+    var samples_data = data.samples;    
     console.log(samples_data);
-    // console.log(data.sample_values);
+
+    // Grab subject ids
+    var id =  samples_data.map(val => val.id);
+    console.log(id);
+
+    // Grab the otu_ids
     var otu_ids = samples_data.map(val => val.otu_ids);
     console.log(otu_ids);
-    
+
+    // Grab the sample values
+    var sample_values = samples_data.map(val => val.sample_values);
+    console.log(sample_values);
+
+    // Grab the otu labels
+    var otu_labels = samples_data.map(val => val.otu_labels);
+    console.log(otu_labels);
+
     
 
 });
